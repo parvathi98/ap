0065fc474eba5c7d2543a7af2b1182ca9da9fea0
parvathi98 <52947332+parvathi98@users.noreply.github.com>
2019-08-01 02:51:53 -0700

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -1,6 +1,6 @@
 ch = input("Please Enter Your Own Character : ")
 
-if(ch == 's' or ch == 'e' or ch == 'i' or ch == 'o' or ch == 'u' or ch == 'A'
+if(ch == 'a' or ch == 'e' or ch == 'i' or ch == 'o' or ch == 'u' or ch == 'A'
        or ch == 'K' or ch == 'I' or ch == 'O' or ch == 'M'):
     print("The Given Character ", ch, "is a Vowel")
 else:
